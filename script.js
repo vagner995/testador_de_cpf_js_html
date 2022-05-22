41696e1d76a41792ba78264be03e3490913c0761
vagner995 <vagner995@hotmail.com>
2022-05-22 13:55:36 -0300

    Programa feito apenas utilizando um exemplo errado do calculo de cpf, mas solução original. -Mudanças no script.js.

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 console.log('teste');
 
-function validaCPF () {
-    return false;
+function validaCPF (cpf) {
+    console.log(cpf.length)
+
+    if (cpf.length != 11) {
+        return false;
+    }
+
+    var numeros = cpf.substring(0, 9);
+    var digitos = cpf.substring(9, 11);
+    console.log( numeros);
+    console.log( digitos);
+
+    var soma = 0;
+    for( i=0; i<9; i++) {
+        soma += numeros[i] * (10-i);
+    };
+
+    var digitoVerificador1 = soma%11%10;
+ 
+    numeros = numeros + String(digitoVerificador1)
+
+    for( i=0, soma=0; i<10; i++) {
+        soma += numeros[i] * (i);
+    };
+
+    var digitoVerificador2 = soma%11%10;
+
+    if ( digitoVerificador1 == digitos[0] && digitoVerificador2 == digitos[1]){
+        return true;
+    } else {
+        return false;
+    }    
 }
 
 
